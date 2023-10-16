713d55bcfe2ebce23ca7c6e60fb3d521ef8adb95
Comex <seba.val.garat@gmail.com>
2023-10-16 11:49:26 -0300

    change style details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,29 @@ const Navbar = () => {
   return (
     <div>
       <nav>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <NavLink
-            className={setActiveClass}
-            to="/"
-          >
-            {" "}
-            Home{" "}
-          </NavLink>
+        <div className="row">
+          <div className="col-6">
+            <img className="nav-img" src="../../public/assets/img/w-ubicacion.png" alt="" />
+          </div>
+          <div className="col-6">
+            <div className="row justify-end">
+              <NavLink
+                className={setActiveClass}
+                to="/"
+              >
+                {" "}
+                Home{" "}
+              </NavLink>
 
-          <NavLink
-            className={setActiveClass}
-            to="/admin"
-          >
-            {" "}
-            Pokemones{" "}
-          </NavLink>
+              <NavLink
+                className={setActiveClass}
+                to="/admin"
+              >
+                {" "}
+                Pokemones{" "}
+              </NavLink>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
